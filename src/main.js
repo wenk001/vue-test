29530496200d942c8edfb61ea934934cefa22325
wenk001 <1109243952@qq.com>
2019-04-15 18:05:12 +0800

    sss

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import Antd from 'ant-design-vue'
 import "ant-design-vue/dist/antd.css";
+import VueRouter from 'vue-router'
+
+
 Vue.config.productionTip = false
 
 Vue.use(Antd)
+Vue.use(VueRouter)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,80 +1,202 @@
 <template>
-  <a-card>
-    <div>
+<div>
+<div style="margin: 16px 0 16px 10px;">
+  <a-button
+        type="primary"
+        icon="plus"
+      >
+        添加用户
+      </a-button>
+      <a-button
+        type="primary"
+        @click="start"
+        :disabled="!hasSelected"
+        :loading="loading"
+        style="margin-left:10px;"
+      >
+        批量操作
+      </a-button>
+      <span style="margin-left: 8px">
+        <template v-if="hasSelected">
+          {{`选择了 ${selectedRowKeys.length} 条记录`}}
+        </template>
+      </span>
+</div>
+
+  <a-table :columns="columns"
+    :dataSource="data"
+    :pagination="pagination"
+    :loading="loading"
+    :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
+    bordered
+  >
+  
+  </a-table>
+  <div>
+  <h1>按条件查询:</h1>
+     <div>
        <a-form layout="inline">
-        <a-row :gutter="20">
+        <a-row :gutter="10">
 
           <a-col :span="5">
+            <a-card>
             <a-form-item label="账号">
-              <a-input placeholder="请输入账号查询"></a-input>
+              <a-input placeholder="账号查询"></a-input>
             </a-form-item>
+            </a-card>
           </a-col>
 
           <a-col :span="5">
+            <a-card>
             <a-form-item label="性别">
-              <a-select placeholder="请选择性别查询">
-                <a-select-option value="0">请选择性别查询</a-select-option>
+              <a-select placeholder="性别查询">
                 <a-select-option value="1">男性</a-select-option>
                 <a-select-option value="2">女性</a-select-option>
               </a-select>
             </a-form-item>
+            </a-card>
           </a-col>
 
-            <a-col :span="5">
-              <a-form-item label="邮箱">
-                <a-input placeholder="请输入邮箱查询"></a-input>
-              </a-form-item>
-            </a-col>
+            
 
             <a-col :span="5">
-              <a-form-item label="手机号码">
-                <a-input placeholder="请输入手机号码查询"></a-input>
-              </a-form-item>
-            </a-col>
-
-            <a-col :span="5">
+              <a-card>
               <a-form-item label="状态">
-                <a-select placeholder="请选择用户状态查询">
-                  <a-select-option value="0">请选择用户状态</a-select-option>
+                <a-select placeholder="状态查询">
                   <a-select-option value="1">正常</a-select-option>
                   <a-select-option value="2">解冻</a-select-option>
                 </a-select>
               </a-form-item>
+              </a-card>
+            </a-col>
+
+            <a-col :span="5">
+              <a-card>
+              <a-form-item label="手机">
+                <a-input placeholder="手机号码查询"></a-input>
+              </a-form-item>
+              </a-card>
             </a-col>
 
         </a-row>
-        <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
+        <div style="width:172px;margin:10px 40% 10px 40%;overflow: hidden;" class="table-page-search-submitButtons">
               <a-button type="primary" icon="search">查询</a-button>
               <a-button type="primary" icon="reload" style="margin-left: 8px">重置</a-button>
-            </span>
+            </div>
       </a-form>
     </div>
-  </a-card>
+
+
+  </div>
+</div>
+
 </template>
+
 <script>
-export default {
+const columns = [{
+  title: '账号',
+  dataIndex: 'username',
+  sorter: true,
+  width: '11%',
+  scopedSlots: { customRender: 'username' },
+}, {
+  title: '真实姓名',
+  dataIndex: 'name',
+  sorter: true,
+  width: '11%',
+  scopedSlots: { customRender: 'name' },
+}, {
+  title: '性别',
+  dataIndex: 'gender',
+  sorter: true,
+  filters: [
+    { text: '男', value: 'male' },
+    { text: '女', value: 'female' },
+  ],
+  scopedSlots: { customRender: 'gender' },
+  width: '11%',
+}, {
+  title: '邮箱',
+  dataIndex: 'email',
+  width: '11%',
+}, {
+  title: '手机号码',
+  dataIndex: 'phone',
+  key: 'phone',
+  width: '11%',
+}, {
+  title: '状态',
+  dataIndex: 'status',
+  sorter: true,
+  width: '11%',
+  scopedSlots: { customRender: 'status' },
+  filters: [
+    { text: '正常', value: 'normal' },
+    { text: '冻结', value: 'freeze' },
+  ],
+}, {
+  title: '注册时间',
+  dataIndex: 'createTime',
+  sorter: true,
+  width: '11%',
+  scopedSlots: { customRender: 'createTime' },
+}, {
+  title: '修改时间',
+  dataIndex: 'updateTime',
+  sorter: true,
+  width: '11%',
+  scopedSlots: { customRender: 'updateTime' },
+}, {
+  title: '操作',
+  dataIndex: 'move',
+  width: '11%',
+}];
 
+const data = [];
+for (let i = 0; i < 5; i++) {
+  data.push({
+    key: i,
+    username: `wk ${i}`,
+    name: `温凯 ${i}`,
+    gender: '男',
+    phone: '12123132132',
+    email: '111111111@qq.com',
+    status: '正常',
+    createTime: '2019-04-14',
+    updateTime: '2019-04-15',
+  });
 }
-
+export default {
+  data() {
+    return {
+      data,
+      pagination: {},
+      selectedRowKeys: [],
+      loading: false,
+      columns,
+    }
+  },
+   computed: {
+    hasSelected() {
+      return this.selectedRowKeys.length > 0
+    }
+  },
+  methods: {
+    start () {
+      this.loading = true;
+      // ajax request after empty completing
+      setTimeout(() => {
+        this.loading = false;
+        this.selectedRowKeys = [];
+      }, 1000);
+    },
+    onSelectChange (selectedRowKeys) {
+      this.selectedRowKeys = selectedRowKeys
+    }
+    
+  },
+}
 </script>
 <style scoped>
-  .ant-card-body .table-operator{
-    margin-bottom: 18px;
-  }
-  .ant-layout-content{
-    margin:12px 16px 0 !important;
-  }
-  .ant-table-tbody .ant-table-row td{
-    padding-top:15px;
-    padding-bottom:15px;
-  }
-  .anty-row-operator button{margin: 0 5px}
-  .ant-btn-danger{background-color: #ffffff}
 
-  .ant-modal-cust-warp{height: 100%}
-  .ant-modal-cust-warp .ant-modal-body{height:calc(100% - 110px) !important;overflow-y: auto}
-  .ant-modal-cust-warp .ant-modal-content{height:90% !important;overflow-y: hidden}
-
-  .anty-img-wrap{height:25px;position: relative;}
-  .anty-img-wrap > img{max-height:100%;}
 </style>
